5706293fc7f7eaa857f6a49d9519855c5cf4e614
Ibrahim Rahimli <ibrahim.rahimli20016@gmail.com>
2024-07-06 14:35:06 +0400

    First working version of COMPLETE CODE button

--- commands.js
+++ commands.js
@@ -1,5 +1,5 @@
 const vscode = require('vscode');
-const { queryHuggingFaceAPI, adjustTextLength, processApiResponseForExplainCode } = require('./api');
+const { queryHuggingFaceAPI, adjustTextLength, processApiResponseForCompleteCode, processApiResponseForExplainCode } = require('./api');
 const { showWebview, getFunctionText } = require('./utils');
 
 function registerCommands(context) {
@@ -27,7 +27,67 @@ function registerCommands(context) {
         }
     });
 
-    context.subscriptions.push(explainCodeCommand);
+    let completeCodeCommand = vscode.commands.registerCommand('ERROR.completeCode', async (args) => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor && args && args.range && typeof args.range.start === 'object' && editor.document) {
+            const document = editor.document;
+            const result = await getFunctionText(document, args);
+            const functionText = result.text;
+            if (functionText) {
+                
+                // Prepare the completion text
+                const completionText = `\n# Complete the code:\n`;
+                const newText = `${functionText}${completionText}`;
+
+                const document = editor.document;
+                const language = document.languageId
+                
+                let comment;
+
+                if (language == 'python'){
+                    comment = '#'; 
+                }else if(language == 'javascript'){
+                    comment = '//';
+                }
+
+                // console.log(module.languageId);
+                console.log(editor.document.languageId)
+
+                try {
+                    const apiResult = await queryHuggingFaceAPI({inputs: newText});
+                    if (apiResult) {
+                        const completedCode = processApiResponseForCompleteCode(apiResult);
+                        // console.log('API Result:', completedCode);
+                        vscode.window.showInformationMessage('Complete Code: See results in the current file.');
+                        const endLine = args.range.start.line + functionText.split('\n').length;
+                        const insertPosition = new vscode.Position(endLine, 0); // Adjust as necessary to place correctly
+
+                        const FinalForm = `\n${comment}Completed code\n${completedCode}\n`;
+            
+                        editor.edit(editBuilder => {
+                            editBuilder.insert(insertPosition, FinalForm);
+                        }).then(success => {
+                            if (success) {
+                                vscode.window.showInformationMessage('Completion prompt added to the function.');
+                            } else {
+                                vscode.window.showErrorMessage('Failed to insert completion prompt.');
+                            }
+                        });
+                    } else {
+                        vscode.window.showErrorMessage('No result from API');
+                    }
+                } catch (error) {
+                    vscode.window.showErrorMessage('Failed to explain code: ' + error.message);
+                }
+
+            } else {
+                console.error('Failed to extract function code');
+                vscode.window.showErrorMessage('Failed to execute Complete Code: Unable to extract function code.');
+            }
+        }
+    });
+
+    context.subscriptions.push(explainCodeCommand, completeCodeCommand);
 }
 
 module.exports = {
